b231c638ac4f872ffd2d07deee3b15dcd5ce45a5
Alex <ipilot93@gmail.com>
2020-10-16 13:04:14 +0400

    trash command

--- Client/Commands/TrashCommands/ExcellentGameCommand.cs
+++ Client/Commands/TrashCommands/ExcellentGameCommand.cs
@@ -11,4 +11,18 @@ namespace PochinkiBot.Client.Commands.TrashCommands
             return userMessage.Channel.SendMessageAsync("https://i.imgur.com/YUMQAmt.jpg");
         }
     }
+
+    [Command("помойка?")]
+    public class TrashServerCommand : IBotCommand
+    {
+        public Task Execute(SocketUserMessage userMessage, int argsPos)
+        {
+            return userMessage.Channel.SendMessageAsync(@"вы похожи на даунов все
+диск в помойку превратился
+и стример админы только и могут что банить всех просто так
+я хуй знает, может это власть так на вас действует
+но почитать что было раньше и что сейчас
+помойка");
+        }
+    }
 }
