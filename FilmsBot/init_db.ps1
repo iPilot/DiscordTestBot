2476a6773f438a8b060f6b84859bac7ceafcd4e6
iPilot <ipilot93@gmail.com>
2022-04-07 14:56:46 +0400

    films bot removed

--- FilmsBot/init_db.ps1
+++
@@ -1,2 +0,0 @@
-docker run --name postgres-bots -p 5432:5432 -e POSTGRES_USER=ipilot -e POSTGRES_PASSWORD=local_postgres -e POSTGRES_DB=postgres -e PGDATA=/var/lib/postgresql/data/pgdata -d -v C:\LocalDb\postgres:/var/lib/postgresql/data postgres
-[Console]::ReadKey()
